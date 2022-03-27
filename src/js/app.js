6c1c1135e23c8efd23d2bd3068cb67d99243766d
facundocejas2 <63550423+facundocejas2@users.noreply.github.com>
2022-03-27 04:46:54 -0300

    upload

---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(){
+    eventListeners();
+
+    darkMode();
+});
+
+function darkMode() {
+
+    const prefiereDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+    //console.log(prefiereDark.matches);
+
+    if (prefiereDark.matches) {
+        document.body.classList.add("dark-mode");
+        
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+
+
+    const botonDarkMode = document.querySelector(".dark-mode-boton");
+
+    botonDarkMode.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    })
+    
+}
+
+function eventListeners() {
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    mobileMenu.addEventListener("click", navegacionResponsive)
+
+
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector(".navegacion");
+
+    navegacion.classList.toggle("mostrar")
+}
